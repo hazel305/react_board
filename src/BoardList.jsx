d75819dfaea9d687b85d5945e25b71d19587306d
Hyunny <hazel413@naver.com>
2023-10-06 13:04:10 +0900

    [FEAT]조회기능 구현

--- src/BoardList.jsx
+++ src/BoardList.jsx
@@ -6,18 +6,46 @@ import axios from "axios";
 
 
 
-const submitTest= ()=>{
-    axios.get('http://localhost:4000/')
-    .then(response=>{
-        alert('연결성공!');
-        console.log(response);
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+class Board extends Component{
+    render(){
+        return(    
+        <tr>
+            <td>{this.props.id}</td>
+            <td>{this.props.title}</td>
+            <td>{this.props.REGISTER_ID}</td>
+            <td>{this.props.REGISTER_DATE}</td>
+        </tr>
+        )
+    }
 }
 class BoardList extends Component{
+    state ={
+        boardList:[]
+    }
+   
+    getList =()=>{
+        axios.get('http://localhost:4000/list')
+        .then((result)=>{
+          const {data} = result;
+          this.setState({
+            boardList:data
+          })
+         
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
+
+    componentDidMount(){
+      this.getList();
+      console.log(this.state.boardList);
+    }
+
+
     render(){
+        // this.getList();
+        // console.log(this.state.boardList);
       return (
         <>
          <Table striped bordered hover>
@@ -30,27 +58,15 @@ class BoardList extends Component{
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+         {this.state.boardList.map((item)=>{
+            return(
+            <Board key={item.BOARD_ID} id={item.BOARD_ID} title={item.BOARD_TITLE} REGISTER_ID={item.REGISTER_ID} REGISTER_DATE={item.REGISTER_DATE}/>
+            )
+         })}
         </tbody>
       </Table>
       <div className='d-flex gap-3'>
-        <Button variant="info" onClick={submitTest}>글쓰기</Button>
+        <Button variant="info" >글쓰기</Button>
         <Button variant="secondary">수정</Button>
         <Button variant="danger">삭제</Button>
       </div>
